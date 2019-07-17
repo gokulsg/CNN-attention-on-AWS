a3976b8338a8158e8cc3460eff3b1765e10c93e0
xiaoya zhang <xiaoya@rainbow.iis.u-tokyo.ac.jp>
2019-07-17 18:33:43 +0900

    Update data_cleaning.py

--- data_cleaning/data_cleaning.py
+++ data_cleaning/data_cleaning.py
@@ -1,7 +1,6 @@
 #created by xiaoya @ tokyo July 12th 2019
 # edit by xiaoya on July 16th 
 import pandas as pd
-from src.const import base_path
 
 
 class merge_onehotvector_columns:
@@ -11,12 +10,13 @@ class merge_onehotvector_columns:
     def __init__(self,attr_min_num,info_file,merge_column_file):
         self.attr_min_num=attr_min_num
         self.file=info_file
-        self.merge_column_file= merge_column_file#'/Users/zhang.xiaoya/Desktop/re_attributes.txt'
+        self.merge_column_file= merge_column_file
         # image selection
         self.df=pd.read_csv(self.file)
         self.df=self.df.loc[self.df[['attr_{}'.format(n) for n in range(1000)]].sum(axis=1)>self.attr_min_num]
         # not yet re-index 
         # answer: less than 2 attri are 48021 
+        # answer : more than 2 attri are between 90000-100000
         self.df.reset_index(inplace=True)
         
     def merge_dic(self):
@@ -39,5 +39,5 @@ class merge_onehotvector_columns:
                 self.df['attr_{}'.format(i)]=0
             if count%10==0:
                 print (count)
-        self.df.to_csv(base_path+'info_processed_0717_biggerthan{}.csv'.format(self.attr_min_num),index=False)
+        self.df.to_csv('info_processed.csv'.format(self.attr_min_num),index=False)
         print('finished merging for',self.df.shape[0],'images')
